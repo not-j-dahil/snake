d6b262b6e3dbf5de8fea5eea10eb6a78034c64dc
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-02 18:36:55 +0100

    created movement of snake logically next: change canvas to show snake moving

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+MOVE_INCREMENT = 20
+
 class Snake(tk.Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
@@ -32,6 +34,17 @@ class Snake(tk.Canvas):
 
         self.create_image(*self.food_position, image=self.food, tag= "food")
         self.create_rectangle(7,27,593,613, outline="#999999")
+    
+    def move_snake(self):
+        head_x_pos, head_y_pos = self.snake_positions[0]
+        new_head_pos = (head_x_pos + MOVE_INCREMENT, head_y_pos)
+
+        self.snake_positions = [new_head_pos] + self.snake_positions[:-1]
+
+        for segment, position in zip(self.find_withtag("snake"), self.snake_positions):
+            self.coords(segment, position)
+
+
 
 
 root = tk.Tk()

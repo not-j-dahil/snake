3a76c1598271a755131d848bde69bd22ba4455fc
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-02 18:01:03 +0100

    imported assets used pipenv to install pillow added "Scripts" into PATH to user pipenv

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 class Snake(tk.Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
+
+        self.load_assets()
+
+    def load_assets(self):
+        try:
+            self.snake_body_image = Image.open("./assets/snake.png")
+            self.snake_body = ImageTk.PhotoImage(self.snake_body_image)
+            
+            self.food_image = Image.open("./assets/food.png")
+            self.food = ImageTk.PhotoImage(self.food_image)
+        except IOError as error:
+            print(error)
+            root.destroy()
 
 root = tk.Tk()
 root.title("Snake")

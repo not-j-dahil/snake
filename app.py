2ebce988b57609b7801f8cd63dee32c3060c1aa0
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-02 17:03:21 +0100

    created canvas

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
-root = tk.Tk()
+class Snake(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, background="black", highlightthickness=0)
 
+root = tk.Tk()
+root.title("Snake")
+root.resizable(False, False)
+
+board = Snake()
+board.pack()
 
 #start window
 root.mainloop()
